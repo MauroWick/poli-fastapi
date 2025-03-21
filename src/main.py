bdea64a4d3432281bb074cf8238a495a46168fbc
Mauro <mauro@maurowick.com>
2025-03-21 08:13:23 -0300

    adicionado fake_alunos e filtros de api

--- src/main.py
+++ src/main.py
@@ -8,6 +8,8 @@ from pydantic import BaseModel
 from mangum import Mangum
 import pandas as pd
 
+from fake_alunos import FAKE_ALUNOS
+
 
 app = FastAPI()
 
@@ -25,7 +27,7 @@ class Aluno(BaseModel):
     nome: str
     email: str
     universidade: str
-    curso: int
+    curso: str
     ano_graduacao: int
     telefone: str
     cidade: str
@@ -38,51 +40,47 @@ class Aluno(BaseModel):
     autoriza_dados: bool
 
 
-fake_aluno = Aluno(
-    nome="John Doe",
-    email="johndoe@example.com",
-    universidade="Universidade Exemplo",
-    curso=1,
-    ano_graduacao=2024,
-    telefone="123456789",
-    cidade="Cidade Exemplo",
-    estado="Estado Exemplo",
-    pais="Pais Exemplo",
-    cpf="12345678900",
-    modalidade_estagio="Remoto",
-    competencias=["Python", "FastAPI"],
-    ja_estagiou=True,
-    autoriza_dados=True
-)
-
-fake_alunos = [
-    Aluno(
-        nome=f"John Doe {i}",
-        email=f"johndoe{i}@example.com",
-        universidade="Universidade Exemplo",
-        curso=1,
-        ano_graduacao=2024,
-        telefone=f"12345678{i}",
-        cidade="Cidade Exemplo",
-        estado="Estado Exemplo",
-        pais="Pais Exemplo",
-        cpf=f"1234567890{i}",
-        modalidade_estagio="Remoto",
-        competencias=["Python", "FastAPI"],
-        ja_estagiou=True,
-        autoriza_dados=True
-    ) for i in range(10)
-]
-
-
 @app.get("/alunos/{aluno_id}", response_model=Aluno)
 async def get_aluno(aluno_id: int):
-    return fake_aluno
+    return FAKE_ALUNOS[0]
 
 
 @app.get("/alunos", response_model=list[Aluno])
 async def get_alunos():
-    return fake_alunos
+    return FAKE_ALUNOS
+
+
+@app.get("/university")
+def get_universities():
+    return {
+    'universities': list({aluno['universidade'] for aluno in FAKE_ALUNOS})
+    }
+
+
+@app.get("/course")
+def get_courses():
+    return {
+    'courses': list({aluno['curso'] for aluno in FAKE_ALUNOS})
+    }
+
+
+@app.get("/skill")
+def get_skills():
+    skills = []
+    for aluno in FAKE_ALUNOS:
+        skills.extend(aluno['competencias'])
+    return {
+        'skills': skills
+    }
+
+
+@app.get("/filter_options")
+def get_filter_options():
+    return {
+        'universities': list({aluno['universidade'] for aluno in FAKE_ALUNOS}),
+        'courses': list({aluno['curso'] for aluno in FAKE_ALUNOS}),
+        'skills': list({skill for aluno in FAKE_ALUNOS for skill in aluno['competencias']})
+    }
 
 
 @app.post("/upload_spreadsheet")
